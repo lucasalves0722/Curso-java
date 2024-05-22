e8aecdc0b89d15414a85e30375bcf7bab4891c4a
lucasalves0722 <lucasalves098messias@gmail.com>
2024-05-22 02:46:00 -0300

    metodo get e set

---
+++ src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        
+        //ALTERAR OS VALORES
+        pessoa.setNome("Yanca");
+        pessoa.setIdade(24);
+        pessoa.setAltura(1.70);
+        
+        //APRESENTAR OS VALORES
+        System.out.println(pessoa.getNome());
+        System.out.println(pessoa.getIdade());
+        System.out.println(pessoa.getAltura());
+    }
+}
---
+++ src/Pessoa.java
@@ -0,0 +1,32 @@
+public class Pessoa {
+    //Atributos
+    private String nome = "Lucas";
+    private int idade = 20;
+    private double altura = 1.80;
+
+    //Métodos
+    //<modificador-acesso> <tipo-retorno> nomeMetodo()
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+}
